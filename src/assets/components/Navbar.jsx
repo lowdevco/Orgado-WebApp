afdd778a69fd05d3649b3b303ee9a267dc7d8c37
Irfan <muhammadirfank2007@gmail.com>
2025-12-26 15:23:44 +0530

    Minor responsivness fix

--- src/assets/components/Navbar.jsx
+++ src/assets/components/Navbar.jsx
@@ -2,136 +2,133 @@ import React from "react";
 import navbarlogo from "../image/logo.webp";
 import "../css/navbar.css";
 
-
 function Navbar() {
-  
-
   return (
-    <div className="container-fluid" id="navbar">
-      <div
-        className="container-fluid d-flex justify-content-between py-3 align-items-center navbar-container"
-      >
-        <div>
-          <a href="" >
-            <img src={navbarlogo} alt="logo" />
-          </a>
-        </div>
+    <nav className="navbar">
+      <div className="container-fluid" id="navbar">
+        <div className="container-fluid d-flex justify-content-between py-3 align-items-center navbar-container">
+          <div>
+            <a href="">
+              <img src={navbarlogo} alt="logo" />
+            </a>
+          </div>
 
-        <div className="d-flex justify-content-evenly gap-4 align-items-center">
-          <div className="d-flex ">
-            <h6>Home</h6>
-          </div>
-          <div className="d-flex">
-            <h6>
-              Shop{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-chevron-down"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-                />
-              </svg>
-            </h6>
-          </div>
-          <div className="d-flex">
-            <h6>
-              Pages{" "}
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-chevron-down"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-                />
-              </svg>
-            </h6>
-          </div>
-          <div>
-            <h6>Blog</h6>
-          </div>
-          <div>
-            <h6>Contact</h6>
-          </div>
-          <div>
-            <h6>Track Order</h6>
-          </div>
-        </div>
-
-        <div id="search-bar">
-          <div className="d-flex">
-            <div>
-              <input type="text" />
+          <div className="d-flex justify-content-evenly gap-4 align-items-center">
+            <div className="d-flex ">
+              <h6>Home</h6>
+            </div>
+            <div className="d-flex">
+              <h6>
+                Shop{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-chevron-down"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                  />
+                </svg>
+              </h6>
+            </div>
+            <div className="d-flex">
+              <h6>
+                Pages{" "}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-chevron-down"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                  />
+                </svg>
+              </h6>
             </div>
             <div>
-              <button>
+              <h6>Blog</h6>
+            </div>
+            <div>
+              <h6>Contact</h6>
+            </div>
+            <div>
+              <h6>Track Order</h6>
+            </div>
+          </div>
+
+          <div id="search-bar">
+            <div className="d-flex">
+              <div>
+                <input type="text" />
+              </div>
+              <div>
+                <button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    fill="currentColor"
+                    class="bi bi-search"
+                    viewBox="0 0 16 16"
+                  >
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <div className="d-flex justify-content-evenly gap-4">
+            <div className="navbar-icon">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 width="16"
                 height="16"
                 fill="currentColor"
-                class="bi bi-search"
+                class="bi bi-bag"
                 viewBox="0 0 16 16"
               >
-                <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0" />
+                <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
               </svg>
-              </button>
             </div>
-          </div>
-        </div>
 
-        <div className="d-flex justify-content-evenly gap-4">
-          <div className="navbar-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-bag"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 1a2.5 2.5 0 0 1 2.5 2.5V4h-5v-.5A2.5 2.5 0 0 1 8 1m3.5 3v-.5a3.5 3.5 0 1 0-7 0V4H1v10a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2V4zM2 5h12v9a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1z" />
-            </svg>
-          </div>
+            <div className="navbar-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-heart"
+                viewBox="0 0 16 16"
+              >
+                <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
+              </svg>
+            </div>
 
-          <div className="navbar-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-heart"
-              viewBox="0 0 16 16"
-            >
-              <path d="m8 2.748-.717-.737C5.6.281 2.514.878 1.4 3.053c-.523 1.023-.641 2.5.314 4.385.92 1.815 2.834 3.989 6.286 6.357 3.452-2.368 5.365-4.542 6.286-6.357.955-1.886.838-3.362.314-4.385C13.486.878 10.4.28 8.717 2.01zM8 15C-7.333 4.868 3.279-3.04 7.824 1.143q.09.083.176.171a3 3 0 0 1 .176-.17C12.72-3.042 23.333 4.867 8 15" />
-            </svg>
-          </div>
-
-          <div className="navbar-icon">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="16"
-              height="16"
-              fill="currentColor"
-              class="bi bi-person"
-              viewBox="0 0 16 16"
-            >
-              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
-            </svg>
+            <div className="navbar-icon">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-person"
+                viewBox="0 0 16 16"
+              >
+                <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z" />
+              </svg>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </nav>
   );
 }
 
--- src/assets/components/Subbar.jsx
+++ src/assets/components/Subbar.jsx
@@ -3,53 +3,59 @@ import "../css/subbar.css";
 
 function Subbar() {
   return (
-    <div className="container-fluid" id="subbar">
-      <div className="container-fluid d-flex justify-content-between py-2 gap-3 subbar-container" >
-        <div className="d-flex justify-content-evenly gap-3">
-          <div>
-            <a href="" id="sub-link">About Us</a>
+    <header className="subbar">
+      <div className="container-fluid" id="subbar">
+        <div className="container-fluid d-flex justify-content-between py-2 gap-3 subbar-container">
+          <div className="d-flex justify-content-evenly gap-3">
+            <div>
+              <a href="" id="sub-link">
+                About Us
+              </a>
+            </div>
+            <div>|</div>
+            <div>
+              <a href="" id="sub-link">
+                My account
+              </a>
+            </div>
           </div>
-          <div>|</div>
-          <div>
-            <a href="" id="sub-link">My account</a>
-          </div>
-        </div>
 
-        <div className="d-flex justify-content-evenly gap-3">
-          <div id="option">
-            <select name="language" id="language">
-              <option value="EN">English</option>
-              <option value="HN">Hindi</option>
-              <option value="AR">Arabic</option>
-              <option value="BE">Bengali</option>
-              <option value="FR">French</option>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                class="bi bi-chevron-down"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
-                />
-              </svg>
-            </select>
-          </div>
-          <div>|</div>
-          <div id="option">
-            <select name="currency" id="currency">
-              <option value="USD">USD</option>
-              <option value="EUR">EUR</option>
-              <option value="JPY">JPY</option>
-              <option value="GBP">GBP</option>
-            </select>
+          <div className="d-flex justify-content-evenly gap-3">
+            <div id="option">
+              <select name="language" id="language">
+                <option value="EN">English</option>
+                <option value="HN">Hindi</option>
+                <option value="AR">Arabic</option>
+                <option value="BE">Bengali</option>
+                <option value="FR">French</option>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  class="bi bi-chevron-down"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708"
+                  />
+                </svg>
+              </select>
+            </div>
+            <div>|</div>
+            <div id="option">
+              <select name="currency" id="currency">
+                <option value="USD">USD</option>
+                <option value="EUR">EUR</option>
+                <option value="JPY">JPY</option>
+                <option value="GBP">GBP</option>
+              </select>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </header>
   );
 }
 
